8ffe9a8282e25ea8949a35badd08a5cc880a1301
Abdulboriy To'lqinov <148456574+Genius4090@users.noreply.github.com>
2025-02-12 16:03:23 +0500

    upfdpdfpf

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -26,26 +26,9 @@ function Modal({ selected, closeModal, setDownloadLink, handleAddToCollection })
   };
 
   const handleDownload = () => {
-    // Fetch the data.json file from the public/data/ directory
-    fetch('/data/data.json')
-      .then(response => response.json())
-      .then(data => {
-        // Find the correct image title from data.json
-        const imageData = data.find(img => img.orig === selected.orig);
-        const fileName = imageData ? imageData.title.split(" ").join("_") + ".jpg" : "default.jpg";
-  
-        // Create a link element, set its download attribute and click it to trigger the download
-        const link = document.createElement('a');
-        link.href = selected.orig;
-        link.download = fileName;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      })
-      .catch(error => console.error('Error fetching data.json:', error));
+    const fileName = selected.title.split(" ").join("_") + ".jpg"; 
+    setDownloadLink({ image: selected.orig, fileName });
   };
-  
-  
 
   const handleCloseModal = () => {
     setIsClosing(true);
